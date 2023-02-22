d0dc8e87388188c37decb2d3cfc8d188349d32f1
Sad-creature <yana.kravchuk.2000@gmail.com>
2023-02-22 03:24:00 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,6 @@ import FullReload from 'vite-plugin-full-reload';
 export default defineConfig({
   root: 'src',
   build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'src/index.html'),
-        restaurant: resolve(__dirname, 'src/restaurant.html'),
-        hotel: resolve(__dirname, 'src/hotel.html'),
-        room: resolve(__dirname, 'src/room.html'),
-      },
-    },
     outDir: '../dist',
   },
   plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
