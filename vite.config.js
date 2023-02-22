ed0388941b1a1a579ce9861bebc4ed2048f0b0dd
Sad-creature <yana.kravchuk.2000@gmail.com>
2023-02-22 02:20:20 +0200

    Add transitions through pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,14 @@ import FullReload from 'vite-plugin-full-reload';
 export default defineConfig({
   root: 'src',
   build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        restaurant: resolve(__dirname, 'src/restaurant.html'),
+        hotel: resolve(__dirname, 'src/hotel.html'),
+        room: resolve(__dirname, 'src/room.html'),
+      },
+    },
     outDir: '../dist',
   },
   plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
